97583ed0056a87919f47cd66c6a5a09c6fcce92a
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2023-08-30 00:18:46 +0300

    fix: add needed sql queries

--- void_parking.sql
+++ void_parking.sql
@@ -1,1 +1,6 @@
+CREATE TABLE IF NOT EXISTS `parkingmeters` (
+  `coords` varchar(50) DEFAULT NULL,
+  `rotation` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 ALTER TABLE `player_vehicles` ADD COLUMN `coords` TEXT NULL DEFAULT NULL
